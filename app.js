5ad7e4cdbd923c7d57149ed31a585cf1292c49b9
asmagala <asmagala@op.pl>
2023-06-20 14:58:30 +0200

    Add procedure for adding hearings to database.

--- app.js
+++ app.js
@@ -27,23 +27,50 @@ const config = {
 /////////////////////////////////////////////////////////////////////////
 app.get('/edit-hearing', (req, res) => {
   sql.connect(config, () => {
-    res.render('edit-hearing', {
-      pageTitle: 'Dodawanie i edycja rozpraw',
-      rozprawa: [
-        {
-          id: '2',
-          name: 'Nazwa rozprawy',
-          description: 'Długa nazwa rozprawy'
-         }, {
-          id: '3',
-          name: 'Nazwa rozprawy drugiej',
-          description: 'Długa nazwa rozprawy drugiej'
-          }
-        ]
+    var hearings = new sql.Request();
+    hearings.query('SELECT * FROM simple_v_hearing', function(hearingErr, hearingResult) {
+      if (hearingErr) {
+        console.log(hearingErr);
+        return res.status(500).send('Error retrieving simple hearings');
+      }
 
+      // Render the hearing page with the list of buildings and locations
+      res.render('edit-hearing', {
+        pageTitle: 'Rozprawy',
+        hearings: hearingResult.recordset
+        });
     });
   });
 });
+
+// insert the submitted form data into the locations table
+app.post('/edit-hearing', (req, res) => {
+  console.log('req.body: ', req.body);
+  const link = req.body.link;
+  const hearing_date = req.body.hearing_date;
+  const time_from = req.body.time_from;
+  const time_to = req.body.time_to;
+  const info = req.body.info;
+  const signature = req.body.signature;
+  const location = req.body.location;
+
+  sql.connect(config, (err) => {
+    if (err) throw err;
+
+    const request = new sql.Request();
+    const query = `INSERT INTO simple_v_hearing (link, hearing_date, time_from, time_to, info, signature, location) VALUES ('${link}', '${hearing_date}', '${time_from}', '${time_to}', '${info}', '${signature}', '${location}')`;
+
+    console.log('query: ', query);
+
+    request.query(query, (err, result) => {
+      if (err) throw err;
+
+      console.log('Hearing added successfully!');
+      res.redirect('/edit-hearing');
+    });
+  });
+});
+
 
 
 
